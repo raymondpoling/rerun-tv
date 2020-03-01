92177cecdd0843d7be149b76e9b5913b0f55ffcf
Raymond Poling <ruguerv@gmail.com>
2020-03-01 12:18:47 -0500

    Updated interface and added logging.
    Also moved to common-lib.

--- src/playlist_schedule/handler.clj
+++ src/playlist_schedule/handler.clj
@@ -5,14 +5,11 @@
             [compojure.route :as route]
             [ring.util.response :refer [response not-found header status]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [common-lib.core :as clc]
+            [clojure.tools.logging :as logger]
             [db.db :refer :all]
             [org.httpkit.server :refer [run-server]])
   (:gen-class))
-
-(defn make-response [st resp]
-  (-> (response resp)
-      (status st)
-      (header "content-type" "application/json")))
 
 (defroutes app-routes
   (POST "/:name" [name]
@@ -21,28 +18,31 @@
           (if (nil? (find-schedule name))
             (let [sched (make-schedule-from-json (:body request))]
               (insert-schedule name sched)
-              (make-response 200 {"status" "ok"}))
-            (make-response 400 {:message (str name " is already defined.")}))
+              (clc/make-response 200 {"status" "ok"}))
+            (clc/make-response 400 {:status :failed :message (str name " is already defined.")}))
           (catch Exception e
-            (make-response 412 {:message (ex-data e)})))))
+            (logger/error (str "Cannot save schedule '" name "': " (.getMessage e)))
+            (clc/make-response 412 {:status :failed :message (ex-data e)})))))
   (GET "/:name" [name]
     (if (not (nil? (find-schedule name)))
-      (make-response 200 (find-schedule name))
-      (route/not-found (str "Not Found " name))))
+      (clc/make-response 200 {:status :ok, :schedule (find-schedule name)})
+      nil))
   (GET "/:name/:index" [name index]
-    (make-response 200 (frame (make-schedule-from-json (find-schedule name)) (Integer/parseInt index))))
+    (clc/make-response 200 {:status :ok :items (frame (make-schedule-from-json (find-schedule name)) (Integer/parseInt index))}))
   (PUT "/:name" [name]
       (fn [request]
         (try
           (let [sched (make-schedule-from-json (:body request))]
             (update-schedule name sched)
-            (make-response 200 {"status" "ok"}))
+            (clc/make-response 200 {"status" "ok"}))
             (catch Exception e
-              (make-response 412 {:message (:message (ex-data e))})))))
+              (logger/error (str "Could not update schedule '" name "': " (.getMessage e)))
+              (clc/make-response 412 {:status :failed :message (:message (ex-data e))})))))
   (DELETE "/:name" [name]
     (delete-schedule name)
-    (make-response 200 {"status" "ok"}))
-  (route/not-found "Not Found"))
+    (clc/make-response 200 {"status" "ok"}))
+  (route/not-found
+    (clc/make-response 404 {:status :not-found})))
 
 (def app
   (wrap-defaults

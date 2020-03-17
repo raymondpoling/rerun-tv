307ec8352b8ce85a0bd9e7e050d03e1349ebe3d1
Raymond Poling <ruguerv@gmail.com>
2020-03-16 20:04:24 -0400

    Made cosmetic changes

--- src/html/schedule_builder_get.clj
+++ src/html/schedule_builder_get.clj
@@ -22,9 +22,11 @@
         [:form {:method "post" :action "schedule-builder.html" :class "box"}
           [:h2 {:class "box-center"} "Update Schedule"]
           (vec (concat (list :select {:name "schedule-name" :class "box-center"}) (schedule-options schedule-names)))
+          [:input {:type "hidden" :name "preview" :value "true"}]
           [:input {:type "submit" :name "type" :value "Update" :class "box-center"}]]
         [:form {:method "post" :action "schedule-builder.html" :class "box"}
           [:h2 {:class "box-center"} "Create Schedule"]
           [:input {:type "text" :name "schedule-name" :class "box-center"}]
+          [:input {:type "hidden" :name "preview" :value "true"}]
           [:input {:type "submit" :name "type" :value "Create" :class "box-center"}]]
         [:div {:id "message" :style (if (not message) "display:none")} (if message message)]]]))

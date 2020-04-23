1712c025e4aefa777304b1653fcbbffaf6f9f77f
Raymond Poling <ruguerv@gmail.com>
2020-04-23 12:53:47 -0400

    Audit: pylinting single_series.py

--- scripts/series_playlists.py
+++ scripts/series_playlists.py
@@ -1,48 +1,64 @@
 #!/usr/bin/python
 
-from urllib import quote
+'''A script that will ensure every series from meta has an associated
+playlist.'''
+
+from urllib.parse import quote
 import requests
 
-playlist_server = "http://playlist:4001"
-meta_server = "http://meta:4004"
+PLAYLIST_SERVER = "http://playlist:4001"
+META_SERVER = "http://meta:4004"
 
-system_postfix = ":SYSTEM"
+SYSTEM_POSTFIX = ":SYSTEM"
 
 def get_all_series():
-    request = requests.get(url = meta_server + "/series")
+    '''Get all series known to meta service'''
+    request = requests.get(url=META_SERVER + "/series")
     json = request.json()
-    print "all series: " + str(json)
+    print("all series: " + str(json))
     return json["results"]
 
-def is_playlist_defined(playlist_name,playlist):
-    request = requests.get(url = playlist_server + "/" +
-                           quote(playlist_name,safe=''))
+def is_playlist_defined(playlist_name, playlist):
+    '''Does a playlist exist for this series?'''
+    request = requests.get(url=PLAYLIST_SERVER + "/" +
+                           quote(playlist_name, safe=''))
     json = request.json()
     if 'items' in json.keys():
-        print "Is " + str(playlist) + " == " + str(json['items']) + "? " + str(playlist == json['items'])
+        print("Is " + str(playlist) + " == " +
+              str(json['items']) + "? " +
+              str(playlist == json['items']))
         return playlist == json['items']
-    else:
-        return False
+    return False
 
 def get_playlist(series):
-    request = requests.get(url = meta_server + "/series/" + quote(series,safe=''))
+    '''Get the elements that should make up a playlist.'''
+    request = requests.get(url=META_SERVER +
+                           "/series/" +
+                           quote(series, safe=''))
     json = request.json()
-    print "playlist: " + str(json) + " for series " + quote(series,safe='')
+    print("playlist: " + str(json) + " for series " + quote(series, safe=''))
     return json["catalog_ids"]
 
-def save_playlist(series,playlist):
-    request = requests.put(url = playlist_server + "/" + quote(series + system_postfix, safe=''), json = {"playlist":playlist})
-    print "Put for series '" + series + "' was: " + str(request.json())
-    if (request.json()["status"] != "ok"):
-        r2 = requests.post(url = playlist_server + "/" + quote(series + system_postfix,safe=''), json = {"playlist":playlist})
-        print "Post for series '" + series + "' was: " + str(r2.json())
+def save_playlist(series, playlist):
+    '''Save a new playlist'''
+    request = requests.put(url=PLAYLIST_SERVER + "/" +
+                           quote(series + SYSTEM_POSTFIX, safe=''),
+                           json={"playlist":playlist})
+    print("Put for series '" + series + "' was: " + str(request.json()))
+    if request.json()["status"] != "ok":
+        other_request = requests.post(url=PLAYLIST_SERVER + "/" +
+                                      quote(series + SYSTEM_POSTFIX, safe=''),
+                                      json={"playlist":playlist})
+        print("Post for series '" + series + "' was: " +
+              str(other_request.json()))
 
 def main():
+    '''Run the script'''
     all_series = get_all_series()
     for series in all_series:
         playlist = get_playlist(series)
-        print "series: " + series + "\n\tplaylist: " + str(playlist)
-        if(not is_playlist_defined(series + system_postfix, playlist)):
-            save_playlist(series,playlist)
+        print("series: " + series + "\n\tplaylist: " + str(playlist))
+        if not is_playlist_defined(series + SYSTEM_POSTFIX, playlist):
+            save_playlist(series, playlist)
 
 main()
